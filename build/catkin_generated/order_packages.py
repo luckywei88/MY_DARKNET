f9a7760ceaa2f6cce5b603b1b7a9c4390523f1ca
luckywei88 <1010442593@qq.com>
2017-09-23 22:24:03 +0800

    no change

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/lucky/MyLib/darknet/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/lucky/workspace/devel;/opt/ros/indigo".split(';') if "/home/lucky/workspace/devel;/opt/ros/indigo" != "" else []
